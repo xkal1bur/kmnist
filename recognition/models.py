b3544791d8de68cc03ab2f62e335e0fccf01bf8d
arturombc <arturomagno1@gmail.com>
2025-02-25 03:20:21 -0500

    models 2

--- recognition/models.py
+++ recognition/models.py
@@ -41,7 +41,7 @@ class Net(nn.Module):
 
 # Carga del modelo y pesos (se hace una sola vez)
 model = Net()
-model.load_state_dict(torch.load('recognition/modelo_kanji_1500000_completo_40.pth'))
+model.load_state_dict(torch.load('recognition/modelo_kanji_1500000_completo_40.pth', map_location=torch.device('cpu')))
 model.eval()
 
 # Diccionario de mapeo de índices a símbolos Kanji
