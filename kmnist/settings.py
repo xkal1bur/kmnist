70961106740649c1155aad8f840e36e3aee0d59d
arturombc <arturomagno1@gmail.com>
2025-02-25 19:23:44 -0500

    please? 3

--- kmnist/settings.py
+++ kmnist/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PORT = os.getenv('PORT', 8000)
 
 
 # Quick-start development settings - unsuitable for production
